9defa46eb39ad2cb372ffdd58a7c857a6b23acf2
RafaFigueiroa <rafatinsfig@gmail.com>
2024-06-09 00:39:40 -0300

    base do projeto

---
+++ src/main/java/com/rafafig/curriculo_springboot/CurriculoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.rafafig.curriculo_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculoSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafafig/curriculo_springboot/CurriculoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafafig.curriculo_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
